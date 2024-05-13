8e156997f744a57f3f2676d0b0e8c1aa4ec4386f
Sharif101 <sharif009009@gmail.com>
2024-05-13 12:19:00 +0600

    add code

--- src/Chatbot/ActionProvider.jsx
+++ src/Chatbot/ActionProvider.jsx
@@ -2,25 +2,24 @@ import React from "react";
 
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
   // ------------------------
-  const afterAgeMessageadult = (e) => {
-    const message = createChatBotMessage(`Wow !!! you are${e}, you adult !!`);
+
+  const initialAction = () => {
+    const message = createChatBotMessage("Just type in your name to begin.");
+    updateState(message, "age");
+  };
+  const afterNameMessage = (e) => {
+    const message = createChatBotMessage(`Welcome ${e}. What is your age?`);
     updateState(message);
   };
-
   const afterAgeMessagekid = (e) => {
     const message = createChatBotMessage(
       `Ha Ha Ha !!! you are${e}, you still kid go to home now`
     );
     updateState(message);
   };
-
-  const afterNameMessage = (e) => {
-    const message = createChatBotMessage(`Welcome ${e}. What is your age?`);
+  const afterAgeMessageadult = (e) => {
+    const message = createChatBotMessage(`Wow !!! you are${e}, you adult !!`);
     updateState(message);
-  };
-  const initialAction = () => {
-    const message = createChatBotMessage("Just type in your name to begin.");
-    updateState(message, "age");
   };
 
   const updateState = (message, checker = "") => {
